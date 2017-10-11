b8c15d3538993e1ac020e775964a32b7c2d4defd
billylinder <billy@billylinder.com>
2017-10-11 14:38:17 -0400

    Update footer text

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,6 @@
 	<div class="container">	
 		<hr class="featurette-divider">
 		<footer>
-			<p class="float-right">designed by Billy Linder</p>
 			<p>This site is not official and is an assignment for a UCF Digital Media course</p>
 		</footer>
 	</div>
